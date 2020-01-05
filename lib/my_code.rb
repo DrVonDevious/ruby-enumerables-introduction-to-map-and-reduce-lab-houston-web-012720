dccf6cb279f6c9830ce8a39a214af15459f9285a
William C Rutland <william.prometheus@gmail.com>
2020-01-05 15:19:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,63 @@
-# My Code here....
+def map_to_negativize(array)
+  i = 0
+  new_array = []
+  while i < array.length do
+    new_array.push(array[i] - (array[i]*2))
+    i += 1
+  end
+  new_array
+end
+
+def map_to_no_change(array)
+  i = 0
+  new_array = []
+  while i < array.length do
+    new_array.push(array[i])
+    i += 1
+  end
+  new_array
+end
+
+def map_to_double(array)
+    i = 0
+  new_array = []
+  while i < array.length do
+    new_array.push(array[i] * 2)
+    i += 1
+  end
+  new_array
+end
+
+def map_to_square(array)
+    i = 0
+  new_array = []
+  while i < array.length do
+    new_array.push(array[i] ** 2)
+    i += 1
+  end
+  new_array
+end
+
+def reduce_to_total(array, starting_point = 0)
+  i = 0
+  total = 0 + starting_point
+  while i < array.length do
+    total += array[i]
+    i += 1
+  end
+  total
+end
+
+def reduce_to_all_true(array)
+  i = 0
+  while i < array.length do
+    unless array[i] == true || array[i] == 1
+      array.delete_at(i) 
+    end
+    i += 1
+  end
+  array
+end
+
+def reduce_to_any_true(array)
+end
