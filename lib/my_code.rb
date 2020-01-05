7f68cd36a00de230b9f23e4fbb009a664d861039
William C Rutland <william.prometheus@gmail.com>
2020-01-05 15:20:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,7 +51,7 @@ end
 def reduce_to_all_true(array)
   i = 0
   while i < array.length do
-    unless array[i] == true || array[i] == 1
+    unless array[i] == true or array[i] == 1
       array.delete_at(i) 
     end
     i += 1
