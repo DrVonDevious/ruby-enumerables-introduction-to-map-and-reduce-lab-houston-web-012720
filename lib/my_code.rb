3b74f6f01f9b17b54e47d54a94d3620d13d4d824
William C Rutland <william.prometheus@gmail.com>
2020-01-05 15:46:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,19 +49,7 @@ def reduce_to_total(array, starting_point = 0)
 end
 
 def reduce_to_all_true(array)
-  i = 0
-  result = 0
-  while i < array.length do
-    if array[i] == false
-      result += 1
-    end
-    i += 1
-  end
-  if result > 1
-    return false
-  else
-    return true
-  end
+  return array
 end
 
 def reduce_to_any_true(array)
